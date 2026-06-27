01d374afb0051255671d1efa316cc65c22e7384d
showrya619 <40289521+showrya619@users.noreply.github.com>
2026-06-27 16:07:03 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        prevmap = {}
+        
+        for i,n in enumerate(nums):
+            needed = target - n
+            if needed in prevmap:
+                return [prevmap[needed],i]
+            else:
+                prevmap[n] = i
+
+        
